6feb184cac108fc23368f3d3b36a539b215a3617
Rai Tilosava D <raidearaujo06@gmail.com>
2023-05-26 18:28:29 +0700

    mengganti message error

--- rest-api/app/Helpers/MessageError.php
+++ rest-api/app/Helpers/MessageError.php
@@ -3,12 +3,15 @@
     function messageError($message) {
       if(is_array($message)) {
         $responseError = '';
-        
-        foreach($message as $key => $value) {
-          $responseError = $key . ": " . $value[0] . ", "; 
+
+        foreach($message as $value) {
+          $responseError =  $value[0]; 
         }
 
-        return response()->json($responseError, 422);
+        return response()->json([
+          'msg' => $responseError,
+          'statusCode' => 422
+        ], 422);
       }
 
       throw new Exception("Message not array typed");
