7a34440acff175c3d00a15775c2e9bb27bde4b2e
Rai Tilosava D <raidearaujo06@gmail.com>
2023-05-31 17:30:24 +0700

    [update] edit-data-siswa

--- frontend/src/app/admin/edit-data-siswa/edit-data-siswa.page.ts
+++ frontend/src/app/admin/edit-data-siswa/edit-data-siswa.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'src/app/service/local-storage.service';
+import { environment } from 'src/environments/environment';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-edit-data-siswa',
@@ -7,9 +11,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditDataSiswaPage implements OnInit {
 
-  constructor() { }
+  id:any;
+  isi:any;
+  constructor(
+    private db:LocalStorageService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastController: ToastController
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id')
+    console.log(this.id);
+    this.getDetail();
   }
 
+  async getDetail(){
+    try {
+      const res = await fetch(environment.urlApi + `api/admin/show-student-data/${this.id}`,{
+        method:"GET",
+        headers:{
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${this.db.get('token')}`
+        }
+      })
+
+      const data = await res.json();
+      this.isi = data.data.data
+      console.log(this.isi);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  doSubmit(){
+    this.editStudent()
+  }
+
+  editStudent(){
+    const data = {
+      "nis": this.isi.nis,
+      "nama": this.isi.nama,
+      "email": this.isi.email
+    }
+
+    fetch(environment.urlApi + `api/admin/update-student-data/${this.id}`,{
+      method:"PUT",
+      body:JSON.stringify(data),
+      headers:{
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${this.db.get('token')}`
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      this.router.navigateByUrl('daftar-siswa')
+    }, err => {
+      console.log(err);
+    })
+  }
 }
